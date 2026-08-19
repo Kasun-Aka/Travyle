804fd4a07069f252245fc40b8dc1e393e7267abd
Kasun Akalanka <upekkasun2003@gmail.com>
2026-08-20 00:56:48 +0530

    Initial Backend update
    folder structure + sample user model

--- backend/Program.cs
+++ backend/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using Travyle.Api.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,6 +8,11 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+builder.Services.AddDbContext<TravyleDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
