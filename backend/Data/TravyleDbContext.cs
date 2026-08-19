8befb60890605c062be640109ad5f1ce23c485d3
Kasun Akalanka <upekkasun2003@gmail.com>
2026-08-19 23:42:54 +0530

    Backend Files added

---
+++ backend/Data/TravyleDbContext.cs
@@ -0,0 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using Travyle.Api.Models;
+
+namespace Travyle.Api.Data;
+
+public class TravyleDbContext : DbContext
+{
+    public TravyleDbContext(DbContextOptions<TravyleDbContext> options) : base(options) { }
+
+    public DbSet<User> Users => Set<User>();
+    // each vertical adds their own DbSets here as they build
+}
